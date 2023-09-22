547d95a61881033c2b5498aa1f7b608adb7c25ce
jschang19 <wwwsean13579@gmail.com>
2023-09-22 23:04:06 +0800

    refactor: separate into 2 handler functions

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,53 @@
-import { http } from "@google-cloud/functions-framework";
+import { http, Request, Response } from "@google-cloud/functions-framework";
 import { Client, validateSignature, WebhookEvent } from "@line/bot-sdk";
 import handleEvent from "./line/handler/index";
 import handleOfferland from "./line/handler/handleOfferland";
 
-import dotenv from "dotenv";
-dotenv.config();
-
 const line = new Client({
-	channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN as string,
-	channelSecret: process.env?.LINE_CHANNEL_SECRET as string,
+	channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN!,
+	channelSecret: process.env.LINE_CHANNEL_SECRET!,
 });
 
-http("main", async (req, res) => {
-	const reqParam = req.originalUrl;
-	const param = reqParam.split("/")[1];
-	switch (param) {
-		case "line":
-			try {
-				// validate signature
-				if (
-					!validateSignature(
-						JSON.stringify(req.body),
-						process.env.LINE_CHANNEL_SECRET as string,
-						req.headers["x-line-signature"] as string,
-					)
-				) {
-					res.status(401).send("Invalid Signature");
-					return;
-				}
+const handleLineRequest = async (req: Request, res: Response) => {
+	try {
+		// validate signature
+		if (
+			!validateSignature(
+				JSON.stringify(req.body),
+				process.env.LINE_CHANNEL_SECRET as string,
+				req.headers["x-line-signature"] as string,
+			)
+		) {
+			res.status(401).send("Invalid Signature");
+			return;
+		}
 
-				const events = req.body.events;
-				const results = await Promise.all(events.map((event: WebhookEvent) => handleEvent(line, event)));
-				res.status(200).send(results);
-			} catch (err: any) {
-				res.status(err.status || 500).send(err.message);
-			}
-			break;
-		case "offerland-trigger":
-			const result = await handleOfferland(line, req.body);
-			res.status(200).send(result);
-			break;
-		default:
-			res.status(404).send("Not Found");
+		const events: WebhookEvent[] = req.body.events;
+		const results = await Promise.all(events.map((event) => handleEvent(line, event)));
+		res.status(200).send(results);
+	} catch (err: any) {
+		res.status(err.status || 500).send(err.message);
+	}
+};
+
+http("main", async (req: Request, res: Response) => {
+	// a middleware to validate the signature
+	try {
+		const reqParam = req.originalUrl;
+		const param = reqParam.split("/")[1];
+		switch (param) {
+			case "line":
+				await handleLineRequest(req, res);
+				break;
+			case "offerland-trigger":
+				const result = await handleOfferland(line, req.body);
+				res.status(200).send(result);
+				break;
+			default:
+				res.status(404).send("Not Found");
+		}
+	} catch (error: any) {
+		console.error("main function error: ", error);
+		res.status(error.status || 500).send(error.message);
 	}
 });
