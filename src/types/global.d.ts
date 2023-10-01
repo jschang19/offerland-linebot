09e7aa8b5570c7e2e8be6b00e3bcee5e4a828136
jschang19 <wwwsean13579@gmail.com>
2023-10-01 22:36:03 +0800

    add: new global type file
    For process env

---
+++ src/types/global.d.ts
@@ -0,0 +1,14 @@
+declare global {
+	namespace NodeJS {
+		interface ProcessEnv {
+			[key: string]: string | undefined;
+			LINE_CHANNEL_SECRET: string;
+			LINE_CHANNEL_ACCESS_TOKEN: string;
+			SUPABASE_URL: string;
+			SUPABASE_SERVICE_KEY: string;
+		}
+	}
+	var supabase: import("@supabase/supabase-js").SupabaseClient;
+}
+
+export {};
