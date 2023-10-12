e31090557cad876239efa8fc6b7b6f7e6377b94e
jschang19 <wwwsean13579@gmail.com>
2023-10-13 01:51:11 +0800

    fix: add ids for result first

--- src/handlers/supabase/tasks/multicast/precise.ts
+++ src/handlers/supabase/tasks/multicast/precise.ts
@@ -1,23 +1,30 @@
-import { Client } from "@line/bot-sdk";
+import { Client, FlexBubble } from "@line/bot-sdk";
 import { createMulitcastGroup } from "@utils/result/groupResult";
+import { assignIdsToResults } from "@utils/result/assignId";
 import { generateSubscribtionCarousel, generateSubscribtionBubble } from "@utils/subscriptionList";
 
 // Multicast to users who subscribe to the same result on Line
 const preciseMulticast = async (line: Client, results: Result[]) => {
+	results = assignIdsToResults(results);
 	const multicastGroups = createMulitcastGroup(results);
 	if (multicastGroups.length === 0) return;
 
 	for (const group of multicastGroups) {
-		const messageBubbles = [];
-		const { resultIds, subscribers } = group;
+		const messageBubbles: FlexBubble[] = [];
+		let { resultIds, subscribers } = group;
 
 		for (let id in resultIds) {
 			const result = results.find((result: Result) => result.id === resultIds[id]);
+			if (!result) {
+				console.error(`Result ${resultIds[id]} not found`);
+				continue;
+			}
 			const bubble = generateSubscribtionBubble(result);
 			messageBubbles.push(bubble);
 		}
 		const carouselMessage = generateSubscribtionCarousel(messageBubbles);
 		await line.multicast(subscribers, carouselMessage);
+		console.log(`Multicast to ${subscribers.length} users`);
 	}
 	return;
 };
