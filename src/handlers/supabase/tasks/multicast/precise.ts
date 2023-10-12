b81486651b761e49a9579aa052e44f1f6719f21e
jschang19 <wwwsean13579@gmail.com>
2023-10-13 02:47:03 +0800

    feat: update user message quota

--- src/handlers/supabase/tasks/multicast/precise.ts
+++ src/handlers/supabase/tasks/multicast/precise.ts
@@ -2,29 +2,39 @@ import { Client, FlexBubble } from "@line/bot-sdk";
 import { createMulitcastGroup } from "@utils/result/groupResult";
 import { assignIdsToResults } from "@utils/result/assignId";
 import { generateSubscribtionCarousel, generateSubscribtionBubble } from "@utils/subscriptionList";
+import updateMultiQuota from "@utils/user/updateQuota";
 
-// Multicast to users who subscribe to the same result on Line
 const preciseMulticast = async (line: Client, results: Result[]) => {
-	results = assignIdsToResults(results);
-	const multicastGroups = createMulitcastGroup(results);
-	if (multicastGroups.length === 0) return;
+	try {
+		results = assignIdsToResults(results);
+		const multicastGroups = createMulitcastGroup(results);
 
-	for (const group of multicastGroups) {
-		const messageBubbles: FlexBubble[] = [];
-		let { resultIds, subscribers } = group;
-
-		for (let id in resultIds) {
-			const result = results.find((result: Result) => result.id === resultIds[id]);
-			if (!result) {
-				console.error(`Result ${resultIds[id]} not found`);
-				continue;
-			}
-			const bubble = generateSubscribtionBubble(result);
-			messageBubbles.push(bubble);
+		if (multicastGroups.length === 0) {
+			return;
 		}
-		const carouselMessage = generateSubscribtionCarousel(messageBubbles);
-		await line.multicast(subscribers, carouselMessage);
-		console.log(`Multicast to ${subscribers.length} users`);
+
+		for (const group of multicastGroups) {
+			const messageBubbles: FlexBubble[] = [];
+			let { resultIds, subscribers } = group;
+
+			resultIds.forEach((id) => {
+				const result = results.find((r: Result) => r.id === id);
+				if (!result) {
+					console.error(`Result ${id} not found!`);
+					return;
+				}
+				const bubble = generateSubscribtionBubble(result);
+				messageBubbles.push(bubble);
+			});
+			const carouselMessage = generateSubscribtionCarousel(messageBubbles);
+			await line.multicast(subscribers, carouselMessage);
+			await updateMultiQuota(subscribers);
+
+			console.log("Precise multicast task finished.");
+			console.log("Finished at: " + new Date().toLocaleString("zh-TW", { timeZone: "Asia/Taipei" }));
+		}
+	} catch (err: any) {
+		console.error("preciseMulticasr error: " + err);
 	}
 	return;
 };
---
+++ src/utils/user/updateQuota.ts
@@ -0,0 +1,12 @@
+const updateMultiQuota = async (users: string[]) => {
+	const { error } = await global.supabase.rpc("update_line_quota", {
+		line_ids: users,
+	});
+
+	if (error) {
+		console.error("updateMultiQuota error: " + error);
+	}
+	return;
+};
+
+export default updateMultiQuota;
