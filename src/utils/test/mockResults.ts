3cd4b0bae3bb42b13061fcd9c34a077d5be31dc1
jschang19 <wwwsean13579@gmail.com>
2023-10-14 03:44:49 +0800

    refactor: update fuc for generating test results

--- src/utils/test/mockResults.ts
+++ src/utils/test/mockResults.ts
@@ -13,9 +13,14 @@ export const mockResults = (subscribers: number[]) => {
 	return results;
 };
 
-export const generateMockResults = (total: number, admitted: number): Result[] => {
+export const generateMockResults = (descion: number, admitted: number, reject: number): Result[] => {
+	const total = descion + admitted + reject;
 	if (admitted > total) {
 		throw new Error("The number of admitted results cannot exceed the total number of results.");
+	}
+
+	if (total < 0 || descion < 0 || admitted < 0) {
+		throw new Error("The number of results cannot be negative.");
 	}
 
 	if (total > 26) {
@@ -25,11 +30,18 @@ export const generateMockResults = (total: number, admitted: number): Result[] =
 	const results: Result[] = [];
 
 	for (let i = 0; i < total; i++) {
-		const isAdmitted = i < admitted;
+		// generate admitted results first
+		// then generate descion results
+		// then generate reject results if any
+
+		const isAdmitted = admitted > 0;
+		const isDescion = descion > 0;
+		const isReject = reject > 0;
+
 		const result: Result = {
 			id: String.fromCharCode(97 + i), // 97 is the ASCII code for 'a'
 			date: new Date().toDateString(),
-			type: isAdmitted ? "admitted" : "rejected", // Adjust this according to your actual types
+			type: isAdmitted ? "admit" : isDescion ? "descion" : isReject ? "reject" : "unknown",
 			user: {
 				name: `User${i}`,
 				graduated_university: `University${i}`,
@@ -62,6 +74,14 @@ export const generateMockResults = (total: number, admitted: number): Result[] =
 		};
 
 		results.push(result);
+
+		if (isAdmitted) {
+			admitted--;
+		} else if (isDescion) {
+			descion--;
+		} else if (isReject) {
+			reject--;
+		}
 	}
 
 	return results;
