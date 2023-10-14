6538f8b3cd1ccc5eb0267f3eeb170ee497478121
jschang19 <wwwsean13579@gmail.com>
2023-10-14 14:53:16 +0800

    perf: using hashmap for results

--- src/handlers/supabase/tasks/multicast/precise.ts
+++ src/handlers/supabase/tasks/multicast/precise.ts
@@ -1,13 +1,14 @@
 import { Client, FlexBubble } from "@line/bot-sdk";
-import { createMulticastGroup } from "@utils/result/groupResult";
-import { assignIdsToResults } from "@utils/result/assignId";
+import { createMulticastGroup, createResultsMap } from "@utils/result/groupResult";
+import { assignIdToResults } from "@utils/result/assignId";
 import { generateSubscribtionCarousel, generateSubscribtionBubble } from "@utils/subscriptionList";
 import updateMultiQuota from "@utils/user/updateQuota";
 
 const preciseMulticast = async (line: Client, results: Result[]) => {
 	try {
-		results = assignIdsToResults(results);
-		const multicastGroups = createMulticastGroup(results);
+		const assignedResults = assignIdToResults(results);
+		const multicastGroups = createMulticastGroup(assignedResults);
+		const resultsMap = createResultsMap(results);
 
 		if (multicastGroups.length === 0) {
 			return;
@@ -18,7 +19,7 @@ const preciseMulticast = async (line: Client, results: Result[]) => {
 			let { resultIds, subscribers } = group;
 
 			resultIds.forEach((id) => {
-				const result = results.find((r: Result) => r.id === id);
+				const result = resultsMap.get(id);
 				if (!result) {
 					console.error(`Result ${id} not found!`);
 					return;
@@ -29,10 +30,11 @@ const preciseMulticast = async (line: Client, results: Result[]) => {
 			const carouselMessage = generateSubscribtionCarousel(messageBubbles);
 			await line.multicast(subscribers, carouselMessage);
 			await updateMultiQuota(subscribers);
-
-			console.log("Precise multicast task finished.");
-			console.log("Finished at: " + new Date().toLocaleString("zh-TW", { timeZone: "Asia/Taipei" }));
 		}
+
+		console.log("Precise multicast task finished.");
+		console.log("Finished at: " + new Date().toLocaleString("zh-TW", { timeZone: "Asia/Taipei" }));
+		console.log("- - -");
 	} catch (err: any) {
 		console.error("preciseMulticasr error: " + err);
 	}
