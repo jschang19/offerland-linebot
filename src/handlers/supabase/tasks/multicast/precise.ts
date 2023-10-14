a35b2077a8e03656d7efb109f23f0ec764f61ab3
jschang19 <wwwsean13579@gmail.com>
2023-10-14 15:38:36 +0800

    refactor: add error handling for precise function

--- src/handlers/supabase/tasks/multicast/precise.ts
+++ src/handlers/supabase/tasks/multicast/precise.ts
@@ -1,4 +1,4 @@
-import { Client, FlexBubble } from "@line/bot-sdk";
+import { Client, FlexBubble, LINE_REQUEST_ID_HTTP_HEADER_NAME } from "@line/bot-sdk";
 import { createMulticastGroup, createResultsMap } from "@utils/result/groupResult";
 import { assignIdToResults } from "@utils/result/assignId";
 import { generateSubscribtionCarousel, generateSubscribtionBubble } from "@utils/subscriptionList";
@@ -28,7 +28,13 @@ const preciseMulticast = async (line: Client, results: Result[]) => {
 				messageBubbles.push(bubble);
 			});
 			const carouselMessage = generateSubscribtionCarousel(messageBubbles);
-			await line.multicast(subscribers, carouselMessage);
+			const lineApiResponse = await line.multicast(subscribers, carouselMessage);
+
+			if (!lineApiResponse[LINE_REQUEST_ID_HTTP_HEADER_NAME]) {
+				console.error("Line API response does not contain request id!");
+				return;
+			}
+
 			await updateMultiQuota(subscribers);
 		}
 
@@ -36,7 +42,7 @@ const preciseMulticast = async (line: Client, results: Result[]) => {
 		console.log("Finished at: " + new Date().toLocaleString("zh-TW", { timeZone: "Asia/Taipei" }));
 		console.log("- - -");
 	} catch (err: any) {
-		console.error("preciseMulticasr error: " + err);
+		console.error("preciseMulticast error: " + err);
 	}
 	return;
 };
