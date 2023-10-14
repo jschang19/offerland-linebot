dfd1944f8f8727ebf6aa496b344453881b191177
jschang19 <wwwsean13579@gmail.com>
2023-10-14 14:54:58 +0800

    refactor: improve modularization for hashmap creation

--- src/utils/result/groupResult.ts
+++ src/utils/result/groupResult.ts
@@ -1,5 +1,10 @@
 const maxResult = 8;
 
+export const createResultsMap = (allResults: Result[]): Map<string, Result> => {
+	return new Map(allResults.map((result) => [result.id, result]));
+};
+
+// 建立各個使用者的訂閱清單
 const mapSubscribersToResults = (results: Result[]): Map<string, string[]> => {
 	const mapping = new Map<string, string[]>();
 
@@ -14,39 +19,37 @@ const mapSubscribersToResults = (results: Result[]): Map<string, string[]> => {
 	return mapping;
 };
 
+// 將同樣訂閱清單的使用者分組
 const groupSubscribersByResults = (subscriptions: Map<string, string[]>): Map<string, string[]> => {
 	const groups = new Map<string, string[]>();
 
-	subscriptions.forEach((results, subscriber) => {
-		const key = results.join(",");
-		const subs = groups.get(key) || [];
-		subs.push(subscriber);
-		groups.set(key, subs);
+	subscriptions.forEach((resultIds, subscriber) => {
+		const idKey = resultIds.join(",");
+		const subscribers = groups.get(idKey) || [];
+		subscribers.push(subscriber);
+		groups.set(idKey, subscribers);
 	});
 
 	return groups;
 };
 
-const transformToArray = (groups: Map<string, string[]>): MulticastGroup[] => {
+const transformMapToArray = (groups: Map<string, string[]>): MulticastGroup[] => {
 	return Array.from(groups.entries()).map(([resultIds, subscribers]) => ({
 		resultIds: resultIds.split(","),
 		subscribers,
 	}));
 };
 
-const getTypeResult = (resultIds: string[], allResults: Result[]): TypeResults => {
+const getTypeResult = (resultIds: string[], resultsMap: Map<string, Result>): TypeResults => {
 	const typeResults: TypeResults = {
 		decision: [],
 		admit: [],
 		reject: [],
 	};
 
-	const resultsMap = new Map(allResults.map((result) => [result.id, result]));
-	console.log("resultIds", resultIds);
 	resultIds.forEach((id) => {
 		const result = resultsMap.get(id);
-		console.log(result?.type);
-		if (result) {
+		if (result && typeResults[result.type]) {
 			typeResults[result.type].push(id);
 		}
 	});
@@ -54,25 +57,36 @@ const getTypeResult = (resultIds: string[], allResults: Result[]): TypeResults =
 	return typeResults;
 };
 
-const sortTypesByDate = (typeResults: TypeResults, allResults: Result[]): TypeResults => {
-	const resultsMap = new Map(allResults.map((result) => [result.id, result]));
+const sortTypesByDate = (typeResults: TypeResults, resultsMap: Map<string, Result>): TypeResults => {
+	const sortedTypeResults: TypeResults = {
+		decision: [],
+		admit: [],
+		reject: [],
+	};
 
+	// 針對 decision, admit, reject 的 result ids 分別依照時間排序
 	Object.entries(typeResults).forEach(([type, resultIds]) => {
-		typeResults[type] = resultIds.sort((a, b) => {
-			const aDate = new Date(resultsMap.get(a)?.date || "").getTime();
-			const bDate = new Date(resultsMap.get(b)?.date || "").getTime();
-			if (aDate === bDate) return 0;
-			return bDate - aDate; // descending order
+		const sortedResultIds = resultIds.sort((a, b) => {
+			const aDate = resultsMap.get(a)?.date;
+			const bDate = resultsMap.get(b)?.date;
+
+			if (!aDate || !bDate) return 0;
+
+			return new Date(bDate).getTime() - new Date(aDate).getTime(); // descending order
 		});
+
+		sortedTypeResults[type as keyof TypeResults] = sortedResultIds;
 	});
 
-	return typeResults;
+	return sortedTypeResults;
 };
 
 export const sortResultId = (multicastGroups: MulticastGroup[], allResults: Result[]): MulticastGroup[] => {
+	// for each group, sort resultIds by date and type
+	const resultsMap = createResultsMap(allResults);
 	return multicastGroups.map((group) => {
-		const typeResults = getTypeResult(group.resultIds, allResults);
-		const sortedTypeResults = sortTypesByDate(typeResults, allResults);
+		const typeResults = getTypeResult(group.resultIds, resultsMap);
+		const sortedTypeResults = sortTypesByDate(typeResults, resultsMap);
 
 		return {
 			...group,
@@ -85,15 +99,12 @@ export const sortResultId = (multicastGroups: MulticastGroup[], allResults: Resu
 };
 
 export const createMulticastGroup = (allResults: Result[]): MulticastGroup[] => {
-	allResults = allResults.filter((result) => result.subscribers.length > 0);
+	const validResults = allResults.filter((result) => result.subscribers.length > 0);
 
-	if (allResults.length === 0) {
-		console.log("Oops, no result to multicast");
-		return [];
-	}
+	if (validResults.length === 0) return [];
 
-	const userSubscriptions = mapSubscribersToResults(allResults);
-	const groupedSubscribers = groupSubscribersByResults(userSubscriptions);
-	const multicastGroups = transformToArray(groupedSubscribers);
-	return sortResultId(multicastGroups, allResults);
+	const userSubscriptionsMap = mapSubscribersToResults(validResults);
+	const multicastGroupsMap = groupSubscribersByResults(userSubscriptionsMap);
+	const multicastGroups = transformMapToArray(multicastGroupsMap);
+	return sortResultId(multicastGroups, validResults);
 };
