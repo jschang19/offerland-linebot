7daccbfd48af31c7ddea7bb5619786ba0ddc1ee4
jschang19 <wwwsean13579@gmail.com>
2023-10-14 14:54:02 +0800

    refactor: rename assginId function

--- src/utils/result/assignId.ts
+++ src/utils/result/assignId.ts
@@ -1,6 +1,6 @@
 import { nanoid } from "nanoid";
 
-export const assignIdsToResults = (results: Result[]): Result[] => {
+export const assignIdToResults = (results: Result[]): Result[] => {
 	return results.map((result) => ({
 		...result,
 		id: result.id ?? nanoid(4),
