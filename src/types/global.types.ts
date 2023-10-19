0d5cab99c2994e9d9ffd99791a9d5bfbce1fe0a8
jschang19 <wwwsean13579@gmail.com>
2023-10-20 01:58:37 +0800

    refactor: Rename type files

--- src/types/global.types.ts
+++ src/types/global.types.ts
@@ -6,6 +6,8 @@ declare global {
 			LINE_CHANNEL_ACCESS_TOKEN: string;
 			SUPABASE_URL: string;
 			SUPABASE_SERVICE_KEY: string;
+			WEBSITE_URL: string;
+			MAIN_COLOR: string;
 		}
 	}
 	var supabase: import("@supabase/supabase-js").SupabaseClient;
