3f0998d526a5028f3e76a08644e237e1bf3bd00c
jschang19 <wwwsean13579@gmail.com>
2023-10-22 02:03:10 +0800

    refactor: Update greeting message

--- src/handlers/line/handleFollow.ts
+++ src/handlers/line/handleFollow.ts
@@ -26,8 +26,11 @@ const handleFollow = async (line: Client, event: FollowEvent) => {
 };
 
 const createFollowReplyMessage = (profile: Profile) => {
-	const welcomeMessage = `Hi ${profile.displayName}!`;
-	const instructionMessage = `綁定帳號後，Offerland 就能給你訂閱校系的新錄取回報通知！\n\n現在馬上綁定帳號吧！`;
+	const welcomeMessage = `${profile.displayName} 你好👋🏻
+	🚀 Land your Offers, OfferLand!
+	留學生＆職場新鮮人都在 OfferLand 上討論！`;
+
+	const instructionMessage = `傳送「 綁定帳號 」兩步驟快速綁定，Offerland 就能給你訂閱校系的新錄取回報通知！現在馬上綁定帳號吧！`;
 
 	return TextMessageWrapper(`${welcomeMessage}\n\n${instructionMessage}`);
 };
