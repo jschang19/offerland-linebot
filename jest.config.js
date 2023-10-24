a46267cd36a150e31a2819c01d494d6abc21bd8d
jschang19 <wwwsean13579@gmail.com>
2023-10-24 10:06:47 +0800

    chore: Update path name mapper in jest config

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,16 @@
+const { pathsToModuleNameMapper } = require("ts-jest");
+// In the following statement, replace `./tsconfig` with the path to your `tsconfig` file
+// which contains the path mapping (ie the `compilerOptions.paths` option):
+const { compilerOptions } = require("./tsconfig");
+
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
 	preset: "ts-jest",
 	testEnvironment: "node",
 	testMatch: ["**/src/test/**/*.test.ts"],
-	moduleNameMapper: {
-		"^(\\.\\/.+)\\.js$": "$1",
-	},
+	roots: ["src"],
+	modulePaths: [compilerOptions.baseUrl], // <-- This will be set to 'baseUrl' value
+	moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths /*, { prefix: '<rootDir>/' } */),
 };
 
 process.env = Object.assign(process.env, {
